1f387850ea48efdba53f74e249e739c747ba7aaf
najwang <anyonanicholas86@gmail.com>
2024-08-25 20:42:53 +0300

    fix: add title to fate layout

---
+++ app/0/layout.js
@@ -0,0 +1,7 @@
+export const metadata = {
+    title: 'XR.MA | fate',
+};
+
+export default function Layout0({children}) {
+    return <>{children}</>;
+}
--- app/0/page.js
+++ app/0/page.js
@@ -1,11 +1,8 @@
 'use client'
 
+import '../css.css'
 import Head from 'next/head';
 import { useEffect } from 'react';
-
-export const metadata= {
-  title: 'XRMA | fate',
-};
 
 export default function Home() {
   useEffect(() => {
@@ -234,6 +231,7 @@ export default function Home() {
   return (
     <>
       <Head>
+        <title>XRMA | fate</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
