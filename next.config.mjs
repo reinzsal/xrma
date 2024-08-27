9fce7acd7219cdf3af70ea2469f06cd08a91b3f2
reinzsal <160291574+reinzsal@users.noreply.github.com>
2024-08-27 12:12:21 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,3 @@ const nextConfig = {};
 
 export default nextConfig;
 
-module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/0/:path*',
-        destination: '/:path*',
-      },
-    ];
-  },
-};
